291a1665c89414c5d282f3fcc2d785199ee33b4f
Danil Buhakov <tradinal@gmai.com>
2018-02-06 16:33:00 +0200

    Overrided onTouchEvent in BoxDrawingView

--- app/src/main/java/com/book/dan/draganddraw/BoxDrawingView.java
+++ app/src/main/java/com/book/dan/draganddraw/BoxDrawingView.java
@@ -1,15 +1,42 @@
 package com.book.dan.draganddraw;
 
 import android.content.Context;
+import android.graphics.PointF;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class BoxDrawingView extends View {
+    private static final String TAG = "BoxDrawingView";
+
     public BoxDrawingView(Context context) {
         super(context);
     }
 
     public BoxDrawingView(Context context, AttributeSet attributeset) {
         super(context,attributeset);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        PointF current = new PointF(event.getX(),event.getY());
+        String action = "";
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                action = "ACTION_DOWN";
+                break;
+            case MotionEvent.ACTION_UP:
+                action = "ACTION_UP";
+                break;
+            case MotionEvent.ACTION_MOVE:
+                action = "ACTION_MOVE";
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                action = "ACTION_CANCEL";
+                break;
+        }
+        Log.i(TAG,action+" at x="+current.x+", y="+current.y);
+        return true;
     }
 }
